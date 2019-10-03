84df13e20ffd5537bbdbecf415d81cb73d6e3d8d
tomoya-kinsho <tomoya.kinsho@bizreach.co.jp>
2019-10-04 01:34:34 +0900

    末尾再帰コード追加

--- learning-scala/src/main/scala/com/github/study/Daily1/Fact.scala
+++ learning-scala/src/main/scala/com/github/study/Daily1/Fact.scala
@@ -2,11 +2,28 @@ package com.github.study.Daily1
 
 class Fact {
 
-  def fact(n: Int): Long = {
-    n match {
-      case 0 => 0
-      case 1 => 1
-      case _ => n * fact(n - 1)
+  // 通常再帰
+  //  def fact(n: Int): Long = {
+  //    n match {
+  //      case 0 => 0
+  //      case 1 => 1
+  //      case _ => n * fact(n - 1)
+  //    }
+  //  }
+
+  // 末尾再帰
+  def fact(n: Int) = {
+    def loop(n: Int, result: Int): Int = {
+      if (n <= 0) result
+      // 4!の場合
+      // loop(3, 4)
+      // loop(2, 12)
+      // loop(1, 24)
+      // loop(0, 24) 
+      else loop(n - 1, n * result)
     }
+
+    loop(n, 1)
   }
+
 }
