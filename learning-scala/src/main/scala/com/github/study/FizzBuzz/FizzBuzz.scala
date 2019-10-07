ed221377eae0cc91355b12727fbd80ae8cd42ebe
tomoya-kinsho <tomoya.kinsho@bizreach.co.jp>
2019-10-08 00:18:56 +0900

    fizzBuzz問題

--- learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
+++ learning-scala/src/main/scala/com/github/study/FizzBuzz/FizzBuzz.scala
@@ -5,6 +5,16 @@ package com.github.study.FizzBuzz
  */
 object FizzBuzz {
   def main(args: Array[String]): Unit = {
-
+    for (i <- 1 to 100)
+      if(i % 3 == 0) {
+        println("Fizz")
+      }  else if(i % 5 == 0) {
+        println("Buzz")
+      } else if(i % 15 == 0) {
+        println("FizzBuzz")
+      } else {
+        println(i)
+      }
   }
 }
+
